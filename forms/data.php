3994e12be7f442b435729d5d7eb526eb7b631178
Utkirbek <utkirbekyuldoshev7@gmail.com>
2023-02-11 17:02:48 +0500

    Myresume website

---
+++ forms/data.php
@@ -0,0 +1,27 @@
+<?php
+    $name = "Utkirbek Yuldoshev";
+    $birthday = "8 oktyabr 1996";
+    $website = "www.MyResume.uz";
+    $phone = "+998 94 526 9896";
+    $city = "Toshkent sh. Yunusobod tumani.";
+    $age = 26;
+    $dager = "Frontend and Backend developer";
+    $email = "utkirbekyuldoshev7@gmail.com";
+    $frelans = "upwork.com";
+
+    $About = "O'zim haqimda";
+    $About_text = "Assalomu allaykum veb saytimga xush kelibsiz, bu veb sayt mening portifolio veb saytim bo'ladi.
+    Bu veb sayt orqali sizni qiziqtirgan loyihalarim ko'rish imkoni mavjud va o'zim haqimda qisqa ma'lumot olishingiz mukin!";
+    $about_titul = "MUHAMMAD AL-XORAZMIY NOMIDAGI TOSHKENT AXBOROT TEXNOLOGIYALARI UNIVERSITETI";
+
+//    Skills
+    $Skills = "Mening ko'nikmalar";
+    $Skills_text = "Bu bo'limda siz men bilgan va o'rganayotgan bilimlarim keltirilgan. 
+    Ko'rishingiz mumkin va batafsil ma'lmotlarim yozgan kodlarim github profilimni ko'rishingiz mumkin!";
+
+//  Aloqa
+    $Contact = "Aloqa uchun";
+
+
+
+?>
